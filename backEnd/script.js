5a87f4ce92425bea937ea06349e44213b1c165b3
joel c <150722950+jo3l4@users.noreply.github.com>
2024-04-09 17:59:51 -0700

    Add files via upload

---
+++ backEnd/script.js
@@ -0,0 +1,10 @@
+function setPriceOnLoad() {
+  // Get the price element
+  var priceElement = document.getElementById('price');
+  
+  // Generate a random price (for demonstration purposes)
+
+  
+  // Update the price element with the new price
+  priceElement.textContent = '10' ;
+}
